f7cd49403ef3a3dcf5d6c0bed9d903b2c39d26c7
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-05-25 19:36:07 -0500

    Got packages installed and now entered code into schema.sql, trying to get database and tables to show in command line

---
+++ schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS staffing_db;
+
+-- creates the "staffing_db" database --
+CREATE DATABASE staffing_db;
+
+-- all of the following code will afffect staffing_db --
+USE staffing_db;
+
+-- Will create the "employees" table in the database we created --
+CREATE TABLE employees (
+ id INT NOT NULL,
+ name VARCHAR(100) NOT NULL
+);
